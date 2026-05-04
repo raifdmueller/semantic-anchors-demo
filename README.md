5feedd3e04ab95966dcb14398c22498541af4c5d
Johannes Rabauer <8188460+JohannesRabauer@users.noreply.github.com>
2026-05-04 17:34:37 +0200

    Init

---
+++ README.md
@@ -0,0 +1,2 @@
+# semantic-anchors-demo
+
---
+++ app/src/main/java/dev/rabauer/example/AppConfig.java
@@ -0,0 +1,8 @@
+package dev.rabauer.example;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.dependency.StyleSheet;
+
+@StyleSheet("styles.css")
+public class AppConfig implements AppShellConfigurator {
+}
